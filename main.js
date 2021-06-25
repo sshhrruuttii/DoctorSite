cbf5c8bf27b1c2ef2b4d6e58d8cd67c791aabbec
sshhrruuttii <56844976+sshhrruuttii@users.noreply.github.com>
2021-06-25 15:48:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+$(".tit-image").hide();
+   $(function() {
+    $(".tit-image").show();
+   $('.tit-image').animate(
+    {top: '150px'}, 2000
+   );
+ });
+ $(".hf").hide();
+ $(function(){
+     $(".hf").fadeIn(3000);
+ })
